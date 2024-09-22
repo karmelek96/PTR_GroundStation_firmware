769ae54133fc8b533694953e1acc0cb28ec420e6
bartekM-pl <144720660+bartekM-pl@users.noreply.github.com>
2024-09-22 18:34:07 +0200

    + Vbat meas on T-beam
    + configuration for LORA32 boards

--- include/BOARD.h
+++ include/BOARD.h
@@ -11,6 +11,12 @@
 
 // Support board list , Macro definition below, select the board definition to be used
 
+//#define LORA32_SX1278
+
+// #define T_BEAM_SX1262
+// #define T_BEAM_SX1276
+#define T_BEAM_SX1278
+
 // #define T3_V1_3_SX1276
 // #define T3_V1_3_SX1278
 
@@ -19,10 +25,6 @@
 
 // #define T3_V1_6_SX1276_TCXO
 // #define T3_V3_0_SX1276_TCXO
-
-// #define T_BEAM_SX1262
-   #define T_BEAM_SX1276
-// #define T_BEAM_SX1278
 
 // #define T_BEAM_S3_SUPREME
 
@@ -575,7 +577,40 @@
 #define BOARD_VARIANT_NAME          "T-Beam BPF"
 
 
+#elif defined(LORA32_SX1278)
 
+#ifndef USING_SX1278
+#define USING_SX1278
+#endif
+
+#define GPS_RX_PIN                  34
+#define GPS_TX_PIN                  12
+#define BUTTON_PIN                  38
+#define BUTTON_PIN_MASK             GPIO_SEL_38
+#define I2C_SDA                     21
+#define I2C_SCL                     22
+
+#define RADIO_SCLK_PIN               5
+#define RADIO_MISO_PIN              19
+#define RADIO_MOSI_PIN              27
+#define RADIO_CS_PIN                18
+#define RADIO_DIO0_PIN              26
+#define RADIO_RST_PIN               23
+#define RADIO_DIO1_PIN              33
+// SX1276/78
+#define RADIO_DIO2_PIN              32
+// SX1262
+#define RADIO_BUSY_PIN              32
+
+#define BAT_ADC_PIN                 35
+#define BOARD_LED                   4
+#define LED_ON                      LOW
+#define LED_OFF                     HIGH
+
+#define GPS_BAUD_RATE               9600
+#define HAS_DISPLAY                 
+
+#define BOARD_VARIANT_NAME          "Lora32"
 
 #else
 #error "When using it for the first time, please define the board model in <utilities.h>"
--- src/Accessories.cpp
+++ src/Accessories.cpp
@@ -1,9 +1,13 @@
+#include "BOARD.h"
 #include "Accessories.h"
 void Accessories_init() {
+#if defined(BAT_ADC_PIN)
     pinMode(BAT_ADC_PIN, INPUT);
+#endif
 }
 
 float Accessories_getVBat() {
+    #if defined(BAT_ADC_PIN)
     uint32_t in = 0;
     for (int i = 0; i < ADC_BATTERY_LEVEL_SAMPLES; i++)
     {
@@ -13,4 +17,7 @@ float Accessories_getVBat() {
     float bat_mv = ((float)in / 4096) * 3600 * 2;
     float bat_v = bat_mv / 1000;
     return bat_v;
+#else
+    return 0.0f;
+#endif
 }
